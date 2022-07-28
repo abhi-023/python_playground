220e43164216a5344ec713af0de24c67bbb67b75
abhi-023 <abhiramrajmoturi23@gmail.com>
2022-07-28 17:30:54 +0530

    add comments

--- basics/dataset.py
+++ basics/dataset.py
@@ -2,4 +2,6 @@ name = "abhiram"
 status = "learning python"
 tool = "PyCharm"
 message = name + " is " + status + " in " + tool
+# returning status
 print(message)
+# TODO: add different examples for dataset
