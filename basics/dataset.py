dcaf67cc61d5e0f144689b1d94366807bb08b805
abhi-023 <abhiramrajmoturi23@gmail.com>
2022-07-28 17:37:50 +0530

    remove dataset

--- basics/dataset.py
+++
@@ -1,7 +0,0 @@
-name = "abhiram"
-status = "learning python"
-tool = "PyCharm"
-message = name + " is " + status + " in " + tool
-# returning status
-print(message)
-# TODO: add different examples for dataset
