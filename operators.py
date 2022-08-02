03fb3bb139ef3f143900a97ce4801471bd2fcfe6
abhi-023 <abhiramrajmoturi23@gmail.com>
2022-08-02 14:41:40 +0530

    namingconv

---
+++ operators.py
@@ -0,0 +1,42 @@
+#  Arithmetic Operator
+a = 9
+b = 4
+add = a + b
+sub = a - b
+mul = a * b
+div1 = a / b
+div2 = a // b
+mod = a % b
+print(add)
+print(sub)
+print(mul)
+print(div1)
+print(div2)
+print(mod)
+#  Relational Operators
+a = 13
+b = 33
+print(a > b)
+print(a < b)
+print(a == b)
+print(a != b)
+print(a >= b)
+print(a <= b)
+# Logical Operator
+a = True
+b = False
+print(a and b)
+print(a or b)
+print(not a)
+#  Bitwise operators
+a = 10
+b = 4
+print(a & b)
+print(a | b)
+print(~a)
+#  XOR operation
+print(a ^ b)
+# print bitwise right shift operation
+print(a >> 2)
+# print bitwise left shift operation
+print(a << 2)
