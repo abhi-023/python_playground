b788877882f6d2b8da6a5351dab6c625a304eb61
abhi-023 <abhiramrajmoturi23@gmail.com>
2022-08-02 13:04:18 +0530

    namingconv

---
+++ namingconv.py
@@ -0,0 +1,12 @@
+# A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+var = "John"
+my_var = "John"
+_var = "John"
+Var = "John"
+VAR = "John"
+var2 = "John"
+
+#Illegal variable names:
+2myvar = "John"
+my-var = "John"
+my var = "John"
